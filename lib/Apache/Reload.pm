3ed43faf369b91cf5e8a17a73af3a38c061a1153
phred <phred@13f79535-47bb-0310-9956-ffa450edef68>
2010-02-09 21:37:20 +0000

    'no Apache::Reload' option to ignore reloading modules with that directive.
    When Moose is being used, reloading a class will probably result in it being unusable.
    
    Especially if the class uses roles or any of the method wrapping calls.
    
    This adds "no Apache::Reload" to make sure that such modules are not attempted to be reloaded.
    
    Submitted by:  Graham Barr <gbarr@pobox.com>
    Signed off by:  Fred Moyer <phred@apache.org>g
    
    
    git-svn-id: https://svn.apache.org/repos/asf/perl/Apache-Reload/trunk@908240 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Apache/Reload.pm
+++ lib/Apache/Reload.pm
@@ -32,6 +32,13 @@ sub import {
     $class->register_module($package, $file);
 }
 
+sub unimport {
+    my $class = shift;
+    my ($package,$file) = (caller)[0,1];
+
+    $class->unregister_module($package, $file);
+}
+
 sub package_to_module {
     my $package = shift;
     $package =~ s/::/\//g;
@@ -56,6 +63,13 @@ sub register_module {
     if (%{"${package}::FIELDS"}) {
         $UndefFields{$module} = "${package}::FIELDS";
     }
+}
+
+sub unregister_module {
+    my ($class, $package, $file) = @_;
+    my $module = package_to_module($package);
+
+    $Ignore{$module} = 1;
 }
 
 sub handler {
@@ -135,9 +149,14 @@ sub handler {
         }
 		# remove the modules
         if ($mtime > $Stat{$file}) {
-            delete $INC{$key};
-            push @changed, $key;
-	 	}
+            if ($Ignore{$key}) {
+                warn "Apache::Reload: Not reloading $key\n";
+            }
+            else {
+                delete $INC{$key};
+                push @changed, $key;
+            }
+        }
         $Stat{$file} = $mtime;
     }
 
@@ -229,6 +248,15 @@ you want to be reloaded on change:
 
 Note that these are split on whitespace, but the module list B<must>
 be in quotes, otherwise Apache tries to parse the parameter list.
+
+=head2 Un-Register Modules Explicitly
+
+If ReloadAll is set to On, then you can explicity force a module not to be reloaded with
+
+  no Apache::Reload;
+
+A warning will appear in the error log that the file has changed, but will
+not be reloaded
 
 =head2 Special "Touch" File
 
